5ebe6f6882b5f22a9b30ff927044341647d941a0
liper <keizokozato@gmail.com>
2025-12-11 19:00:04 -0800

    initial commit

---
+++ src/api/freight-classe/controllers/freight-classe.ts
@@ -0,0 +1,7 @@
+/**
+ * freight-classe controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::freight-classe.freight-classe');
---
+++ src/api/freight-classe/routes/freight-classe.ts
@@ -0,0 +1,7 @@
+/**
+ * freight-classe router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::freight-classe.freight-classe');
